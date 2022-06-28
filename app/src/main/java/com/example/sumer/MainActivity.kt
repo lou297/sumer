5e47d8b1f7e074fe99c4acf293a9c7204c2ac6ca
Lee Jeong Hoon <xorolx297@naver.com>
2022-06-28 16:05:30 +0900

    webView 연동 및 authentication 값 추가

--- app/src/main/java/com/example/sumer/MainActivity.kt
+++ app/src/main/java/com/example/sumer/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.sumer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sumer.util.UrlConstants
+import com.example.sumer.util.LawAIWebViewClient
 import com.example.sumer.databinding.ActivityMainBinding
-import com.example.sumer.databinding.ActivityMainBinding.inflate
 
 class MainActivity : AppCompatActivity() {
     private val binding : ActivityMainBinding by lazy {
@@ -18,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        initWebView()
     }
 
 
@@ -27,23 +27,45 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val webView = binding.webview
+        var webViewUrl = StringBuilder(UrlConstants.LAW_AI_BASE_URL)
         when (item.itemId) {
             R.id.home_btn -> {
-                Toast.makeText(this, "home", Toast.LENGTH_LONG).show()
-                true
+                webViewUrl.append(UrlConstants.HOME)
             }
-            R.id.person_btn -> {
-                Toast.makeText(this, "person", Toast.LENGTH_LONG).show()
-                true
+            R.id.my_page_btn -> {
+                webViewUrl.append(UrlConstants.MY_PAGE)
             }
             R.id.setting_btn -> {
-                Toast.makeText(this, "setting", Toast.LENGTH_LONG).show()
-                true
-            }
-            else -> {
-                false
+                webViewUrl.append(UrlConstants.SETTING)
             }
         }
+
+        webView.loadUrl(webViewUrl.toString())
         return super.onOptionsItemSelected(item)
+    }
+
+    fun initWebView() {
+        val webView = binding.webview
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webView.setPadding(0, 0, 0, 0)
+        webSettings.useWideViewPort = true
+        webSettings.setSupportZoom(true)
+        webSettings.builtInZoomControls = true
+
+        webView.webViewClient = LawAIWebViewClient(this)
+
+        val setting = getSharedPreferences("setting", 0)
+        val cred = setting.getString("cred", "")
+        if ("" == cred == false) {
+            val authHeader = "Basic $cred"
+            val headers: MutableMap<String, String> = HashMap()
+            headers["Authorization"] = authHeader
+            //mWebView.loadUrl(myUrl + "/account/login.mo", headers);
+            webView.loadUrl(UrlConstants.LAW_AI_BASE_URL + "/main.mo", headers)
+        } else {
+            webView.loadUrl(UrlConstants.LAW_AI_BASE_URL + "/main.mo")
+        }
     }
 }
---
+++ app/src/main/java/com/example/sumer/util/UrlConstants.kt
@@ -0,0 +1,8 @@
+package com.example.sumer.util
+
+object UrlConstants {
+    const val LAW_AI_BASE_URL = "https://ai.hklaw.co.kr/"
+    const val HOME = "/main.mo"
+    const val MY_PAGE = "/mine/myPage.mo"
+    const val SETTING = "/setting.mo"
+}
